7dd3d27d3fb2f8da5dd7c625804c38be700b98f4
keremelmaci <keremelmaci06@gmail.com>
2026-05-12 01:21:25 +0300

    Update NamedElement.java

--- src/com/iso15939/model/NamedElement.java
+++ src/com/iso15939/model/NamedElement.java
@@ -1,9 +1,6 @@
 package com.iso15939.model;
 
-/**
- * Base class used to demonstrate inheritance for named project elements.
- * Dimension and Metric are both named, weighted elements.
- */
+
 public abstract class NamedElement {
     private final String name;
     private final double coefficient;
